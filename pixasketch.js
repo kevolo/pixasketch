f7310b9a8992dbbb4543cd8b9ea834e8051bde60
kevolo <klogan@optonline.net>
2024-06-10 09:48:49 -0400

    Added clear and new canvas functionality

--- pixasketch.js
+++ pixasketch.js
@@ -1,12 +1,22 @@
 const canvasDiv = document.getElementById("canvas");
+const clearBtn = document.getElementById("clearBtn");
+const newCanvasBtn = document.getElementById("newCanvasBtn");
 
-const setupGrid = (canvasHeight, canvasWidth) => {
-    console.log("setupGrid");
-    for (let i=0; i<canvasHeight; i++) {
-        for (let j=0; j<canvasWidth; j++) {
+let numRows = 16;
+let numCols = 16;
+let rowHeight = 720 / numRows;
+let colWidth = 960 / numCols;
+
+const setupGrid = (canvasRows, canvasCols) => {
+    //console.log("setupGrid");
+    for (let i=0; i<canvasRows; i++) {
+        for (let j=0; j<canvasCols; j++) {
             const newDiv = document.createElement("div");
-            newDiv.className="pixel";
+            newDiv.className = "pixel";
             newDiv.id = i + "-" + j;
+            newDiv.style.width = `${colWidth}px`;
+            newDiv.style.height = `${rowHeight}px`;
+            newDiv.style.backgroundColor = "darkgray";
             canvasDiv.appendChild(newDiv);
         }
     }
@@ -20,4 +30,18 @@ document.addEventListener('mouseover', function (e) {
     }
 });
 
-setupGrid(16,16);
+clearBtn.addEventListener('click', e => {
+    canvasDiv.replaceChildren();
+    setupGrid(numRows, numCols);
+});
+
+newCanvasBtn.addEventListener('click', e => {
+    canvasDiv.replaceChildren();
+    numRows = prompt("Enter number of rows:", 16);
+    numCols = prompt("Enter number of columns:", 16);
+    rowHeight = 720 / numRows;
+    colWidth = 960 / numCols;
+    setupGrid(numRows, numCols);
+});
+
+setupGrid(numRows, numCols);
