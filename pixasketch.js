80b1b3e6463f50eb480678bb3cbd457dae5a54c5
kevolo <klogan@optonline.net>
2024-06-10 10:52:46 -0400

    Added random color and grayscale rendering modes

--- pixasketch.js
+++ pixasketch.js
@@ -1,11 +1,16 @@
 const canvasDiv = document.getElementById("canvas");
 const clearBtn = document.getElementById("clearBtn");
 const newCanvasBtn = document.getElementById("newCanvasBtn");
+const monoBtn = document.getElementById("monochromeBtn");
+const colorBtn = document.getElementById("randomColorBtn");
+const grayscaleBtn = document.getElementById("grayscaleBtn");
+const modeSpan = document.getElementById("mode");
 
 let numRows = 16;
 let numCols = 16;
 let rowHeight = 720 / numRows;
 let colWidth = 960 / numCols;
+let renderMode = "mono";
 
 const setupGrid = (canvasRows, canvasCols) => {
     //console.log("setupGrid");
@@ -26,7 +31,21 @@ document.addEventListener('mouseover', function (e) {
     if (e.target.className === "pixel") {
         // console.log(e + " : " + e.target + " : " + e.target.id);
         const curPixel = document.getElementById(e.target.id);
-        curPixel.style.backgroundColor = "gray";
+        let pixColor = "gray";
+        if (renderMode === "rand") {
+            pixColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+        } else if (renderMode === "gs") {
+            pixColor = "black";
+        }
+        curPixel.style.backgroundColor = pixColor;
+        if (renderMode === "gs") {
+            let curOpacity = parseInt(curPixel.style.opacity * 100) / 100;
+            if (curOpacity < 1) {
+                let newOpacity = curOpacity + 0.1;
+                newOpacity = newOpacity>1 ? 1 : newOpacity;
+                curPixel.style.opacity = newOpacity;
+            }
+        }
     }
 });
 
@@ -37,11 +56,29 @@ clearBtn.addEventListener('click', e => {
 
 newCanvasBtn.addEventListener('click', e => {
     canvasDiv.replaceChildren();
-    numRows = prompt("Enter number of rows:", 16);
-    numCols = prompt("Enter number of columns:", 16);
+    numRows = parseInt(prompt("Enter number of rows:", 16));
+    numCols = parseInt(prompt("Enter number of columns:", 16));
+    numRows = numRows<2 ? 2 : numRows;
+    numRows = numRows>100 ? 100 : numRows;
+    numCols = numCols<2 ? 2 : numCols;
+    numCols = numCols>100 ? 100 : numCols;
     rowHeight = 720 / numRows;
     colWidth = 960 / numCols;
     setupGrid(numRows, numCols);
+});
+
+monoBtn.addEventListener('click', e => {
+    modeSpan.textContent = "Monochrome";
+    renderMode = "mono";
+});
+
+colorBtn.addEventListener('click', e => {
+    modeSpan.textContent = "Random Color";
+    renderMode = "rand";
+});
+grayscaleBtn.addEventListener('click', e => {
+    modeSpan.textContent = "Grayscale";
+    renderMode = "gs";
 });
 
 setupGrid(numRows, numCols);
