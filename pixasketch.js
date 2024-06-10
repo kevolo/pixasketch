fdb1c2e652bc1f6a7158868604fdf4ea3395f5b7
kevolo <klogan@optonline.net>
2024-06-09 23:42:46 -0400

    Pixel grid rendered and can click pixels to change color now

--- pixasketch.js
+++ pixasketch.js
@@ -6,10 +6,20 @@ const setupGrid = (canvasHeight, canvasWidth) => {
         for (let j=0; j<canvasWidth; j++) {
             const newDiv = document.createElement("div");
             newDiv.className="pixel";
-            newDiv.id = i + "-" + "j";
+            newDiv.id = i + "-" + j;
             canvasDiv.appendChild(newDiv);
         }
     }
+}
+
+document.addEventListener('click', function (e) {
+    // console.log(e + " : " + e.target + " : " + e.target.id);
+    const curPixel = document.getElementById(e.target.id);
+    curPixel.style.backgroundColor = "gray";
+});
+
+const togglePixel = e => {
+    console.log(e + " : " + e.target);
 }
 
 setupGrid(16,16);
